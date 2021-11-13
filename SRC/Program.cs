3ddb9181a671f9e8fe0829291467406409846416
[Vladislav Aulich] <[aulichv@vscht.cz]>
2021-11-13 17:35:33 +0100

    kod_diakritika

--- SRC/Program.cs
+++ SRC/Program.cs
@@ -9,11 +9,11 @@ namespace semestralka_konsole
     class Program
     {
         /// <summary>
-        /// Metoda, která pomocí řady Jamese Gregoryho
-        /// stanový hodnotu Ludolfova čísla.
+        /// Metoda, ktera pomoci rady Jamese Gregoryho
+        /// stanovy hodnotu Ludolfova cisla.
         /// </summary>
-        /// <param name="n">Počet iterací (členů řady)</param>
-        /// <returns>Hodnota pi datového typu decimal</returns>
+        /// <param name="n">Pocet iteraci (clenů rady)</param>
+        /// <returns>Hodnota pi datoveho typu decimal</returns>
         public static decimal VypocitejPi(uint n)
         {
             decimal soucet = 0;
@@ -22,10 +22,10 @@ namespace semestralka_konsole
             {
 
                 pomocna = 4m / (1 + (2 * i));
-                //Pro sudé koeficienty
+                //Pro sude koeficienty
                 if (i % 2 == 0)
                     soucet += pomocna;
-                //Pro liché koeficienty
+                //Pro liche koeficienty
                 else
                     soucet -= pomocna;
             }
@@ -42,14 +42,14 @@ namespace semestralka_konsole
                 try
                 {
                     n = uint.Parse(Console.ReadLine());
-                    //Stanovení podmínky počtu opakování
+                    //Stanovení podminky počtu opakovani
                     if (n > 1000)
                     {
-                        //Upozorní uživatele na špatný vstup
+                        //Upozorni uzivatele na spatny vstup
                         Console.WriteLine("Zadejte prosím celé kladné číslo menší než 1 000");
                     }
                     else
-                        //Správná hodnota n, ukončí smyčku a vypočte pi
+                        //Spravna hodnota n, ukonci smycku a vypocte pi
                         pokracovat = false;
                 }
 
@@ -59,7 +59,7 @@ namespace semestralka_konsole
                 }
             }
             while (pokracovat);
-            //Provedení funkce pro uživatelem zadaný počet iterací a zaokrouhlení na 8 desetinných míst
+            //Provedeni funkce pro uzivatelem zadany pocet iteraci a zaokrouhleni na 8 desetinnych mist
             pi = Math.Round(VypocitejPi(n), 8);
             Console.WriteLine("Hodnota Ludolfova čísla pro {0} iterací je {1}", n, pi);
             Console.ReadKey();
